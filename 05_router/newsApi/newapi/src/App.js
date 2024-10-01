05fc2003b35fe927b4b6ffe3c66f7868ffc975d6
zxczx <zxczx3020@gmail.com>
2024-10-01 18:06:24 +0900

    react

--- 05_router/newsApi/newapi/src/App.js
+++ 05_router/newsApi/newapi/src/App.js
@@ -4,27 +4,7 @@ import { useState } from "react";
 
 function App() {
 
-  const [data,setDate]=useState();
 
-
-
-  const API_KEY = "b2f485cd2f274a5ba62325da31653420"
-
-
-  const getLatesNews = async () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
-    const response = await fetch(url);
-    const data = await response.json()
-    console.log(data);
-
-    setDate(response.data);
-
-  
-  
-  }
-
-  getLatesNews()
-  
 
 
   return(
--- 05_router/newsApi/newapi/src/Newslist/NewsList.js
+++ 05_router/newsApi/newapi/src/Newslist/NewsList.js
@@ -1,14 +1,26 @@
+const [data,setDate]=useState();
+
 
 
 const API_KEY = "b2f485cd2f274a5ba62325da31653420"
 
 
-const getLatesNews = () => {
+const getLatesNews = async () => {
   const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
-  const response = async(url)
-  console.log(response);
+  const response = await fetch(url);
+  const data = await response.json()
+  console.log(data);
+
+  setDate(response.data);
+
 
 
 }
 
-export default getLatesNews();
+getLatesNews()
+
+const item=()=>{
+    const styles={
+        
+    }
+}
